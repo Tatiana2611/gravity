9e2710aa6a2a13a555ba3ae928b8e4b6c92b7262
Tatiana <tati@Air-Tatiana.localdomain>
2020-06-26 20:44:35 +0300

    changes

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+    $('.slider__inner').slick({
+        arrows: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 1,
+        variableWidth: true,
+    });
+
+  });
